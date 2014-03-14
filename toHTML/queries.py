32501d0be6791d34dc648a60ee28d0123d38d256
George Balatsouras <gbalats@gmail.com>
2014-03-14 19:21:09 -0400

    merge

---
+++ toHTML/queries.py
@@ -0,0 +1,289 @@
+MODIFIERS = """
+_(mod) <-
+	meth = "{0}", MethodModifier(mod, meth).
+"""
+
+FORMALS = """
+_(index, type, formal) <-
+	meth = "{0}", FormalParam[index, meth] = formal, Var:Type[formal] = type.
+"""
+
+LOCALS = """
+_(type, local) <-
+	meth = "{0}", _localVar(local, meth), Var:Type[local] = type.
+
+_localVar(local, meth) <-
+	meth = "{0}", Var:DeclaringMethod(local, meth),
+	!FormalParam[_, meth] = local, !ThisVar[meth] = local.
+"""
+
+ALLOCATIONS = """
+_(var, heap) <-
+	meth = "{0}", AssignHeapAllocation(heap, var, meth).
+"""
+
+ASSIGNS = """
+_(to, from) <-
+	meth = "{0}", AssignLocal(from, to, meth).
+"""
+
+CASTS = """
+_(to, type, from) <-
+	meth = "{0}", AssignCast(type, from, to, meth).
+"""
+
+FLD_MODIFIERS = """
+_(fld, mod) <-
+	meth = "{0}",
+	(LoadInstanceField(_, fld, _, meth) ; StoreInstanceField(_, _, fld, meth) ;
+	 LoadStaticField(fld, _, meth) ; StoreStaticField(_, fld, meth)),
+	FieldModifier(mod, fld).
+"""
+
+LOAD_INSTANCE_FIELDS = """
+_(to, base, fld) <-
+	meth = "{0}", LoadInstanceField(base, fld, to, meth).
+"""
+
+LOAD_STATIC_FIELDS = """
+_(to, cls, fld) <-
+	meth = "{0}", LoadStaticField(fld, to, meth), Field:DeclaringClass[fld] = cls.
+"""
+
+STORE_INSTANCE_FIELDS = """
+_(base, fld, from) <-
+	meth = "{0}", StoreInstanceField(from, base, fld, meth).
+"""
+
+STORE_STATIC_FIELDS = """
+_(cls, fld, from) <-
+	meth = "{0}", StoreStaticField(from, fld, meth), Field:DeclaringClass[fld] = cls.
+"""
+
+LOAD_ARRAYS = """
+_(to, base) <-
+	meth = "{0}", LoadArrayIndex(base, to, meth).
+"""
+
+STORE_ARRAYS = """
+_(base, from) <-
+	meth = "{0}", StoreArrayIndex(from, base, meth).
+"""
+
+
+SPECIAL_INV = """
+_t(invo) <-
+	meth = "{0}", SpecialMethodInvocation:In(invo, meth).
+
+_(invo, index, base) -> MethodInvocationRef(invo), int[32](index), VarRef(base).
+
+_(invo, -1, base) <-
+	_t(invo), SpecialMethodInvocation:Base[invo] = base.
+
+_(invo, index, actual) <-
+	_t(invo), ActualParam[index, invo] = actual.
+
+_(invo, -2, ret) <-
+	_t(invo), AssignReturnValue[invo] = ret.
+"""
+
+VIRTUAL_INV = """
+_t(invo) <-
+	meth = "{0}", VirtualMethodInvocation:In(invo, meth).
+
+_(invo, index, base) -> MethodInvocationRef(invo), int[32](index), VarRef(base).
+
+_(invo, -1, base) <-
+	_t(invo), VirtualMethodInvocation:Base[invo] = base.
+
+_(invo, index, actual) <-
+	_t(invo), ActualParam[index, invo] = actual.
+
+_(invo, -2, ret) <-
+	_t(invo), AssignReturnValue[invo] = ret.
+"""
+
+STATIC_INV = """
+_t(invo) <-
+	meth = "{0}", StaticMethodInvocation:In(invo, meth).
+
+_(invo, index, base) -> MethodInvocationRef(invo), int[32](index), VarRef(base).
+
+_(invo, index, actual) <-
+	_t(invo), ActualParam[index, invo] = actual.
+
+_(invo, -2, ret) <-
+	_t(invo), AssignReturnValue[invo] = ret.
+"""
+
+STATIC_INV_NO_VARS = """
+_(invo) <-
+	meth = "{0}", StaticMethodInvocation:In(invo, meth),
+	!AssignReturnValue[invo] = _, !ActualParam[_, invo] = _.
+"""
+
+RETURNS = """
+_(var) <-
+	meth = "{0}", ReturnVar(var, meth).
+"""
+
+
+
+VAR_POINTS_TO = """
+_(var, heap) <-
+	meth = "{0}", Var:DeclaringMethod(var, meth), VarPointsTo(_, heap, _, var).
+
+_(var, dummy) <-
+	meth = "{0}", Var:DeclaringMethod(var, meth), !VarPointsTo(_, _, _, var),
+	MainMethodArgsArray(dummy).
+"""
+
+VAR_POINTS_TO_COUNTS = """
+_(var, 0) <-
+	meth = "{0}", Var:DeclaringMethod(var, meth), !VarPointsTo(_, _, _, var).
+
+_(var, cnt) <- _c[var] = cnt.
+
+_t(var, heap) <-
+	meth = "{0}", Var:DeclaringMethod(var, meth), VarPointsTo(_, heap, _, var).
+
+_c[var] = cnt <- agg<<cnt = count()>> _t(var, _).
+"""
+
+"""VAR_POINTS_TO_COUNTS =
+_(var, 0) <-
+	meth = "{0}", Var:DeclaringMethod(var, meth), !VarPointsTo(_, _, _, var).
+
+_(var, cnt) <- _c[var] = cnt, cnt = 1.
+_(var, cnt) <- _c[var] = cnt, cnt >= 2, _tc[var] >= 2.
+_(var, cnt2) <- _c[var] = cnt, cnt >= 2, _tc[var] = 1, cnt2 = -1 * cnt.
+
+_t(var, heap) <-
+	meth = "{0}", Var:DeclaringMethod(var, meth), VarPointsTo(_, heap, _, var).
+
+_c[var] = cnt <- agg<<cnt = count()>> _t(var, _).
+
+_tt(var, type) <-
+	meth = "{0}", Var:DeclaringMethod(var, meth), VarPointsTo(_, heap, _, var), HeapAllocation:Type[heap] = type.
+
+_tc[var] = cnt <- agg<<cnt = count()>> _tt(var, _).
+"""
+
+FLD_POINTS_TO = """
+_(fld, base, baseHeap, heap) <-
+	meth = "{0}",
+	(LoadInstanceField(base, fld, _, meth) ; StoreInstanceField(_, base, fld, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	InstanceFieldPointsTo(_, heap, fld, _, baseHeap).
+
+_(fld, base, baseHeap, dummy) <-
+	meth = "{0}",
+	(LoadInstanceField(base, fld, _, meth) ; StoreInstanceField(_, base, fld, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	!InstanceFieldPointsTo(_, _, fld, _, baseHeap),
+	MainMethodArgsArray(dummy).
+"""
+
+FLD_POINTS_TO_COUNTS = """
+_(baseHeap, fld, 0) <-
+	meth = "{0}",
+	(LoadInstanceField(base, fld, _, meth) ; StoreInstanceField(_, base, fld, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	!InstanceFieldPointsTo(_, _, fld, _, baseHeap).
+
+_(baseHeap, fld, cnt) <- _c[baseHeap, fld] = cnt.
+
+_t(baseHeap, fld, heap) <-
+	meth = "{0}",
+	(LoadInstanceField(base, fld, _, meth) ; StoreInstanceField(_, base, fld, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	InstanceFieldPointsTo(_, heap, fld, _, baseHeap).
+
+_c[baseHeap, fld] = cnt <- agg<<cnt = count()>> _t(baseHeap, fld, _).
+"""
+
+STATIC_FLD_POINTS_TO = """
+_(cls, fld, heap) <-
+	meth = "{0}",
+	(LoadStaticField(fld, _, meth) ; StoreStaticField(_, fld, meth)),
+	StaticFieldPointsTo(_, heap, fld), Field:DeclaringClass[fld] = cls.
+
+_(cls, fld, dummy) <-
+	meth = "{0}",
+	(LoadStaticField(fld, _, meth) ; StoreStaticField(_, fld, meth)),
+	!StaticFieldPointsTo(_, _, fld), Field:DeclaringClass[fld] = cls,
+	MainMethodArgsArray(dummy).
+"""
+
+STATIC_FLD_POINTS_TO_COUNTS = """
+_(fld, 0) <-
+	meth = "{0}",
+	(LoadStaticField(fld, _, meth) ; StoreStaticField(_, fld, meth)),
+	!StaticFieldPointsTo(_, _, fld).
+
+_(fld, cnt) <- _c[fld] = cnt.
+
+_t(fld, heap) <-
+	meth = "{0}",
+	(LoadStaticField(fld, _, meth) ; StoreStaticField(_, fld, meth)),
+	StaticFieldPointsTo(_, heap, fld).
+
+_c[fld] = cnt <- agg<<cnt = count()>> _t(fld, _).
+"""
+
+ARRAY_POINTS_TO = """
+_(base, baseHeap, heap) <-
+	meth = "{0}",
+	(LoadArrayIndex(base, _, meth) ; StoreArrayIndex(_, base, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	ArrayIndexPointsTo(_, heap, _, baseHeap).
+
+_(base, baseHeap, dummy) <-
+	meth = "{0}",
+	(LoadArrayIndex(base, _, meth) ; StoreArrayIndex(_, base, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	!ArrayIndexPointsTo(_, _, _, baseHeap),
+	MainMethodArgsArray(dummy).
+"""
+
+ARRAY_POINTS_TO_COUNTS = """
+_(baseHeap, 0) <-
+	meth = "{0}",
+	(LoadArrayIndex(base, _, meth) ; StoreArrayIndex(_, base, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	!ArrayIndexPointsTo(_, _, _, baseHeap).
+
+_(baseHeap, cnt) <- _c[baseHeap] = cnt.
+
+_t(baseHeap, heap) <-
+	meth = "{0}",
+	(LoadArrayIndex(base, _, meth) ; StoreArrayIndex(_, base, meth)),
+	VarPointsTo(_, baseHeap, _, base),
+	ArrayIndexPointsTo(_, heap, _, baseHeap).
+
+_c[baseHeap] = cnt <- agg<<cnt = count()>> _t(baseHeap, _).
+"""
+
+VIRTUAL_CALL_GRAPH = """
+_(invo, toMeth) <-
+	meth = "{0}", VirtualMethodInvocation:In(invo, meth), 
+	CallGraphEdge(_, invo, _, toMeth).
+
+_(invo, dummy) <-
+	meth = "{0}", VirtualMethodInvocation:In(invo, meth), 
+	!CallGraphEdge(_, invo, _, _), MainMethodDeclaration(dummy).
+"""
+
+VIRTUAL_CALL_GRAPH_COUNTS = """
+_(invo, 0) <-
+	meth = "{0}", VirtualMethodInvocation:In(invo, meth), 
+	!CallGraphEdge(_, invo, _, _).
+
+_(invo, cnt) <- _c[invo] = cnt.
+
+_t(invo, toMeth) <-
+	meth = "{0}", VirtualMethodInvocation:In(invo, meth), 
+	CallGraphEdge(_, invo, _, toMeth).
+
+_c[invo] = cnt <- agg<<cnt = count()>> _t(invo, _).
+"""
---
+++ toHTML/toHTML.py
@@ -0,0 +1,429 @@
+#!/usr/bin/env python
+
+import argparse, doop, sys, re
+from decimal import *
+
+def cleanVar(var):
+	_, var = var.split("/")
+	return var
+
+def cleanFld(fld):
+	_, fld = fld.split(": ")
+	_, fld = fld.split(" ")
+	return fld[:-1]
+
+def cleanHeap(heap):
+	return heap.replace("<", "").replace(">", "")
+
+def filterHTML(value):
+	return value.replace("<", "&lt;").replace(">", "&gt;")
+
+def printInvocations(res, linkInvocation=False):
+	returnStr = mainStr = ""
+	prev = None
+	for elem in res:
+		parts = elem.split(", ")
+		_, invo = parts[0].split("/", 1)
+		invo = filterHTML(invo)
+		var = genLink( cleanVar(parts[2]) )
+
+		if invo != prev:
+			if prev != None:
+				print genElem( "{0}{1}{2})".format(returnStr, mainStr, ", ".join(actuals)) )
+			returnStr = ""
+			if linkInvocation:
+				mainStr = "{0} (".format(genLink(invo))
+			else:
+				mainStr = "{0} (".format(invo)
+			actuals = []
+			prev = invo
+
+		if parts[1] == "-2":
+			returnStr = "{0} = ".format(var)
+		elif parts[1] == "-1":
+			mainStr = "{0} . {1}".format(var, mainStr)
+		else:
+			actuals.append(var)
+	
+	if prev != None:
+		print genElem( "{0}{1}{2})".format(returnStr, mainStr, ", ".join(actuals)) )
+
+def getColourClass(cnt):
+	if int(cnt) == 0:
+		return "emptyPTSet"
+	elif int(cnt) == 1:
+		return "singlePTSet"
+	elif int(cnt) >= 2:
+		return "multiplePTSet"
+	elif int(cnt) <= -2:
+		return "multiplePTSet singleTypePTSet"
+	else:
+		return "neutral"
+
+def genHeader(title):
+	return "\n<h2>{0}</h2>".format(title)
+
+def genGroupHeader(value, colourClass = "neutral", id = None):
+	id = value if id == None else id
+	return "<h3 class=\"{2}\"><a id=\"{0}\">{1}</a></h3><div>".format(id, value, colourClass)
+
+def genGroupHeader2(value, colourClass):
+	return "<h4 class=\"{1}\">{0}</h4><div>".format(value, colourClass)
+
+def genGroupHeaderEnd():
+	return "</div>"
+
+def genElem(element):
+	return "<div>{0}</div>".format(element)
+
+def genLink(value, id = None):
+	id = value if id == None else id
+	return "<a href=\"#{0}\">{1}</a>".format(id, value)
+
+def genComment(value):
+	return "<span class=\"comment\">// {0}</span>".format(value)
+
+
+HEADER = """<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>{0}</title>
+	<style type="text/css">
+		h2 {{
+			background: Silver;
+			padding: 15px;
+			text-aling: center;
+		}}
+		h3 {{
+			margin-bottom: 0;
+			padding: 10px;
+			border: 1px solid grey;
+		}}
+		h4 {{
+			margin-bottom: 0;
+			padding: 10px;
+			border: 1px solid grey;
+		}}
+		h3 + div, h4 + div {{
+			padding: 10px;
+			border: 1px solid grey;
+			border-top: 0;
+		}}
+		.comment {{
+			margin-left: 20px;
+			color: Grey;
+			font-style: italic;
+		}}
+		.emptyPTSet {{
+			background: Brown;
+		}}
+		.singlePTSet {{
+			background: LightGreen;
+		}}
+		.multiplePTSet {{
+			background: Khaki;
+		}}
+		.singleTypePTSet {{
+			background-image: repeating-linear-gradient(45deg, transparent, transparent 50px, LightGreen 50px, LightGreen 100px);
+		}}
+		.neutral {{
+			background: Silver;
+		}}
+	</style>
+</head>
+<body>
+"""
+
+FOOTER = """
+</body>
+</html>
+"""
+
+def genHTML(db, method):
+	doopconn = doop.Connector(db)
+
+	cl, sig = method[1:-1].split(": ")
+	sig, _ = sig.split("(")
+
+	print HEADER.format(filterHTML(sig))
+
+	res = " ".join( doopconn.modifiers(method) )
+	sig = res + " " + filterHTML(sig) + "("
+
+	res = doopconn.formals(method)
+	res.sort()
+	formals = []
+	for elem in res:
+		parts = elem.split(", ")
+		formal = genLink( cleanVar(parts[2]) )
+		formals.append( "{0} {1}".format(parts[1], formal) )
+	sig += ", ".join( formals ) + ")"
+	print "<h1>[{0}] {1}</h1>".format(cl, sig)
+
+	print genGroupHeader("Local Variables")
+	res = doopconn.locals(method)
+	res.sort()
+	for elem in res:
+		cls, var = elem.split(", ")
+		var = genLink( cleanVar(var) )
+		print genElem( "{0} {1}".format(cls, var) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Allocations")
+	res = doopconn.allocations(method)
+	for elem in res:
+		var, heap = elem.split(", ")
+		var = genLink( cleanVar(var) )
+		heap = cleanHeap( "/".join( heap.split("/")[-2:] ) )
+		print genElem( "{0} = {1}".format(var, heap) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Assigns")
+	res = doopconn.assigns(method)
+	for elem in res:
+		toVar, fromVar = elem.split(", ")
+		toVar = genLink( cleanVar(toVar) )
+		fromVar = genLink( cleanVar(fromVar) )
+		print genElem( "{0} = {1}".format(toVar, fromVar) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Casts")
+	res = doopconn.casts(method)
+	for elem in res:
+		parts = elem.split(", ")
+		toVar = genLink( cleanVar(toVar) )
+		fromVar = genLink( cleanVar(fromVar) )
+		print genElem( "{0} = ({1}) {2}".format(toVar, parts[1], fromVar) )
+	print genGroupHeaderEnd()
+
+	res = doopconn.fldModifiers(method)
+	res.sort()
+	modifiers = {}
+	t = []
+	prev = None
+	for elem in res:
+		fld, mod = elem.split(", ")
+		if prev != fld:
+			if prev != None: modifiers[prev] = " ".join( t )
+			t = []
+			prev = fld
+		t.append(mod)
+	if prev != None: modifiers[prev] = " ".join( t )
+	getMods = lambda x: genComment(modifiers[x]) if x in modifiers else ""
+
+	print genGroupHeader("Load Instance Fields")
+	res = doopconn.loadInstanceFields(method)
+	for elem in res:
+		parts = elem.split(", ")
+		toVar = genLink( cleanVar(parts[0]) )
+		baseVar = cleanVar(parts[1])
+		fld = cleanFld(parts[2])
+		fld = genLink( fld, fld+"$from$"+baseVar )
+		print genElem( "{0} = {1} . {2} {3}".format(toVar, genLink(baseVar), fld, getMods(parts[2])) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Store Instance Fields")
+	res = doopconn.storeInstanceFields(method)
+	for elem in res:
+		parts = elem.split(", ")
+		baseVar = cleanVar(parts[0])
+		fld = cleanFld(parts[1])
+		fld = genLink( fld, fld+"$from$"+baseVar )
+		fromVar = genLink( cleanVar(parts[2]) )
+		print genElem( "{0} . {1} = {2} {3}".format(genLink(baseVar), fld, fromVar, getMods(parts[1])) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Load Static Fields")
+	res = doopconn.loadStaticFields(method)
+	for elem in res:
+		parts = elem.split(", ")
+		toVar = genLink( cleanVar(parts[0]) )
+		fld = cleanFld(parts[2])
+		fld = genLink( fld, fld+"$staticFld$"+parts[1] )
+		print genElem( "{0} = {1} . {2} {3}".format(toVar, parts[1], fld, getMods(parts[2])) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Store Static Fields")
+	res = doopconn.storeStaticFields(method)
+	for elem in res:
+		parts = elem.split(", ")
+		fld = cleanFld(parts[1])
+		fld = genLink( fld, fld+"$staticFld$"+parts[0] )
+		fromVar = genLink( cleanVar(parts[2]) )
+		print genElem( "{0} . {1} = {2} {3}".format(parts[0], fld, fromVar, getMods(parts[1])) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Load Arrays")
+	res = doopconn.loadArrays(method)
+	for elem in res:
+		toVar, fromVar = elem.split(", ")
+		toVar = genLink( cleanVar(toVar) )
+		fromVar = cleanVar(fromVar)
+		print genElem( "{0} = {1} [ {2} ]".format(toVar, genLink(fromVar), genLink("?", fromVar+"$index$")) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Store Arrays")
+	res = doopconn.storeArrays(method)
+	for elem in res:
+		toVar, fromVar = elem.split(", ")
+		toVar = cleanVar(toVar)
+		fromVar = genLink( cleanVar(fromVar) )
+		print genElem( "{0} [ {1} ] = {2}".format(genLink(toVar), genLink("?", toVar+"$index$"), fromVar) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Returns")
+	res = doopconn.returns(method)
+	for elem in res:
+		print genElem( "return {0}".format(genLink(cleanVar(elem))) )
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Special Method Invocations")
+	res = doopconn.specialInv(method)
+	res.sort()
+	printInvocations(res)
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Virtual Method Invocations")
+	res = doopconn.virtualInv(method)
+	res.sort()
+	printInvocations(res, True)
+	print genGroupHeaderEnd()
+
+	print genGroupHeader("Static Method Invocations")
+	res = doopconn.staticInv(method)
+	res.sort()
+	printInvocations(res)
+	res = doopconn.staticInvNoVars(method)
+	for elem in res:
+		_, invo = elem.split("/", 1)
+		print genElem( "{0} ()".format(filterHTML(invo)) )
+	print genGroupHeaderEnd()
+
+
+	print "<div style=\"margin-top: 40px; border: 5px dashed grey;\"></div>"
+	print genHeader("Variables")
+	counts = dict(elem.split(", ") for elem in doopconn.varPointsToCounts(method))
+	res = doopconn.varPointsTo(method)
+	res.sort()
+	prev = None
+	for elem in res:
+		var, heap = elem.split(", ")
+		counter = counts[var]
+		colourClass = getColourClass(counter)
+		var = cleanVar(var)
+		heap = cleanHeap(heap)
+		if prev != var:
+			if prev != None: print genGroupHeaderEnd()
+			print genGroupHeader(var, colourClass)
+			prev = var
+		if counter != "0": print genElem(heap)
+	if res: print genGroupHeaderEnd()
+
+	print genHeader("Fields")
+
+	g1 = lambda x, y: "{0}, {1}".format(x, y)
+	g2 = lambda parts: ( g1(parts[0], parts[1]), parts[2] )
+	counts = dict( g2(elem.split(", ")) for elem in doopconn.fldPointsToCounts(method) )
+	res = doopconn.fldPointsTo(method)
+	res.sort()
+	prev = None
+	prevHeap = None
+	for elem in res:
+		parts = elem.split(", ")
+		fld = cleanFld(parts[0])
+		base = cleanVar(parts[1])
+		id = "{0}$from${1}".format(fld, base)
+		counter = counts[ g1(parts[2], parts[0]) ]
+		colourClass = getColourClass(counter)
+		if prev != id:
+			if prev != None: print genGroupHeaderEnd()
+			print genGroupHeader(base+"."+fld, id = id)
+			prev = id
+			prevHeap = None
+		if prevHeap != parts[2]:
+			if prevHeap != None: print genGroupHeaderEnd()
+			print genGroupHeader2(cleanHeap(parts[2]), colourClass)
+			prevHeap = parts[2]
+		if counter != "0": print genElem( cleanHeap(parts[3]) )
+	if res: print genGroupHeaderEnd() + genGroupHeaderEnd()
+
+	counts = dict(elem.split(", ") for elem in doopconn.staticFldPointsToCounts(method))
+	res = doopconn.staticFldPointsTo(method)
+	res.sort()
+	prev = None
+	for elem in res:
+		parts = elem.split(", ")
+		counter = counts[parts[1]]
+		colourClass = getColourClass(counter)
+		fld = cleanFld(parts[1])
+		id = "{0}$staticFld${1}".format(fld, parts[0])
+		if prev != id:
+			if prev != None: print genGroupHeaderEnd()
+			print genGroupHeader(parts[0]+"."+fld, colourClass, id)
+			prev = id
+		if counter != "0": print genElem( cleanHeap(parts[2]) )
+	if res: print genGroupHeaderEnd()
+	
+	print genHeader("Arrays")
+	counts = dict(elem.split(", ") for elem in doopconn.arrayPointsToCounts(method))
+	res = doopconn.arrayPointsTo(method)
+	res.sort()
+	prev = None
+	prevHeap = None
+	for elem in res:
+		parts = elem.split(", ")
+		_, base = parts[0].split("/")
+		id = "{0}$index$".format(base)
+		counter = counts[parts[1]]
+		colourClass = getColourClass(counter)
+		if prev != id:
+			if prev != None: print genGroupHeaderEnd()
+			print genGroupHeader(base+" [ ? ]", id = id)
+			prev = id
+			prevHeap = None
+		if prevHeap != parts[1]:
+			if prevHeap != None: print genGroupHeaderEnd()
+			print genGroupHeader2(cleanHeap(parts[1]), colourClass)
+			prevHeap = parts[1]
+		if counter != "0": print genElem( cleanHeap(parts[2]) )
+	if res: print genGroupHeaderEnd() + genGroupHeaderEnd()
+
+	print genHeader("Virtual Call Graph")
+	counts = dict(elem.split(", ") for elem in doopconn.virtualCallGraphCounts(method))
+	res = doopconn.virtualCallGraph(method)
+	res.sort()
+	prev = None
+	for elem in res:
+		invo, meth = elem.split(", ")
+		counter = counts[invo]
+		colourClass = getColourClass(counter)
+		_, invo = invo.split("/", 1)
+		if prev != invo:
+			if prev != None: print genGroupHeaderEnd()
+			print genGroupHeader(invo, colourClass)
+			prev = invo
+		if counter != "0": print genElem( cleanHeap(meth) )
+	if res: print genGroupHeaderEnd()
+
+	print FOOTER
+
+
+def main(cmd):
+	if isinstance(cmd, basestring):
+		cmd = cmd.split()
+
+	parser = argparse.ArgumentParser(
+		usage = 'toHTML.py [-h] workspace method')
+
+	parser.add_argument('workspace', 
+		help = 'the db directory of a Doop static analysis')
+	parser.add_argument('method', 
+		help = 'the target method to reconstruct')
+
+	args = parser.parse_args(cmd)
+     
+	genHTML(args.workspace, args.method)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
